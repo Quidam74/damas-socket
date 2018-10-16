691136f57d2e70f9a22c42daada8b13118ecb0b5
Unknown <toutlemonde74@gmail.com>
2018-10-16 08:56:25 +0200

    basic message chat

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Form</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<main class="chat">
+	<h1 class="chat-titre">Mon chat génial</h1>
+	<div class="chat-fenetre"></div>
+	<input class="chat-input" type="text" name="message">
+</main>
+
+	<script src='js/main.js'></script>
+	<script src='js/socket.io.min.js'></script>
+</body>
+</html>
